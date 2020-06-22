391d7c848cb1d0ba43ab1872300d1c57b3e49e97
AbduEth <abdurekmjob@gmail.com>
2020-06-22 10:35:17 -0400

    search function

---
+++ employeeddirectory-app/src/components/SearchForm.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style.css";
+
+function SearchBox(props){
+    return(
+        <div className="contaier-fluid">
+            <header className="z-depth-3 col s12"> 
+            <div className="row">
+
+            </div>
+            <div className="col s6">
+            <h3 className="right-align headerText">Associate Directory</h3>
+                        {/* <h6 className="center-align smallerHeaderText">Use the box down below to narrow your search</h6> */}
+            </div>
+        
+            <div className="col s6">
+                <div className="col s6 inputAndButton right-align">
+                    <input
+                    onChange={props.handleInputChange}
+                    value={props.value}
+                    id="employees"
+                    type="text"
+                    name="search"
+                    list="employee"
+                    className="inputBox"
+                    placeholder="search by name"
+                    />
+                </div>
+                 <div className="col m4">
+                     <button 
+                     type="submit"
+                     value=""
+                     className=" btn z-depth-2 waves-effect  searchBttn"
+                     onClick={props.handleInputChange}> search</button>
+                     
+                 </div>      
+        </div>
+            </header>
+        </div>
+    )
+}
+export default SearchBox
